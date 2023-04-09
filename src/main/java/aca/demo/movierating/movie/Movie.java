225e3bcba714e858c896f1b1a49c601b9a304f81
MiqEghiazaryan <miq.eghiazaryan@gmail.com>
2023-04-09 13:21:25 +0400

    Added class MovieRepository

--- src/main/java/aca/demo/movierating/movie/Movie.java
+++ src/main/java/aca/demo/movierating/movie/Movie.java
@@ -1,15 +1,18 @@
 package aca.demo.movierating.movie;
 
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
+import java.beans.ConstructorProperties;
+
 @Getter
 @ToString
 @EqualsAndHashCode
 @Slf4j
-
+@AllArgsConstructor
 public class Movie {
     String title;
     Genre genre;
@@ -17,4 +20,6 @@ public class Movie {
     Movie(CreateMovie createMovie) {
         log.debug("Constructing movie with createMovie - {}",createMovie);
     }
+
+
 }
---
+++ src/main/java/aca/demo/movierating/movie/MovieRepository.java
@@ -0,0 +1,37 @@
+package aca.demo.movierating.movie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@Component
+@Slf4j
+
+public class MovieRepository {
+
+    List<Movie> movies = new ArrayList<>();
+
+      public Optional<Movie> findByTitle(String title) {
+          log.debug("Finding movie by title: {}", title);
+          return movies.stream()
+                  .filter(movie -> movie.getTitle().equals(title))
+                  .findFirst();
+    }
+
+    public List<Movie> findByGenre(Genre genre) {
+        log.debug("Finding movies by genre: {}", genre);
+        return movies.stream()
+                .filter(movie -> movie.getGenre() == genre)
+                .toList();
+    }
+
+    public void save(CreateMovie createMovie) {
+        log.debug("Creating movie: {}", createMovie);
+        Movie movie = new Movie(createMovie.getTitle(), createMovie.getGenre());
+        movies.add(movie);
+    }
+}
