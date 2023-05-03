bf3b70d3e5d5ec7b1f30b30a0c1a858e7992ef80
MiqEghiazaryan <miq.eghiazaryan@gmail.com>
2023-05-03 12:45:51 +0400

    delete "no usage" imports

--- src/main/java/aca/demo/movierating/endpoint/ReviewController.java
+++ src/main/java/aca/demo/movierating/endpoint/ReviewController.java
@@ -1,5 +1,6 @@
 package aca.demo.movierating.endpoint;
 
+
 import aca.demo.movierating.review.CreateReview;
 import aca.demo.movierating.review.Review;
 import aca.demo.movierating.review.ReviewService;
@@ -9,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 
 @RestController
